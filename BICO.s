c692797fe1f0726baef1ee68b298a5c0f714c7bb
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-17 15:47:53 -0200

    arquivo c

--- BICO.s
+++ BICO.s
@@ -13,4 +13,11 @@ set_motor_speed:
     mov r7, #18
     svc 0x0
 
-    
+set_motors_speed:
+    ldrb r0, [r0, #1]
+    ldrb r1, [r1, #1]
+
+    mov r7, #19
+    svc 0x0
+
+  
---
+++ segue-parede.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "api_robot2.h"
+
+
+int main(void){
+    int i;
+    motor_cfg_t motor0;
+    motor_cfg_t motor1;
+
+    motor0.id = 0;
+    motor0.speed = 40;
+    motor1.id = 1;
+    motor1.speed = 40;
+
+    for(i=0; i<=100;i++){
+        set_motors_speed(motor0, motor1);
+    }
+}
