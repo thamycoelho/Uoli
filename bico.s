409191f6b6f6a3fcb4cc768b506e6de531c5e8d9
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-19 19:14:43 -0200

    arquivo c

--- bico.s
+++ bico.s
@@ -18,8 +18,11 @@ set_motor_speed:
     svc 0x0
 
 set_motors_speed:
-    ldrb r0, [r0, #1]
-    ldrb r1, [r1, #1]
+    ldrb r2, [r0, #1]
+    ldrb r3, [r1, #1]
+
+    mov r0, r2
+    mov r1, r3
 
     mov r7, #19
     svc 0x0
--- segue-parede.c
+++ segue-parede.c
@@ -1,7 +1,6 @@
 #include "api_robot2.h"
 
 int _start(int argv, char** argc){
-    int i;
     motor_cfg_t motor0;
     motor_cfg_t motor1;
 
@@ -10,9 +9,7 @@ int _start(int argv, char** argc){
     motor1.id = 1;
     motor1.speed = 40;
 
-    for(i=0; i<=100;i++){
         set_motors_speed(&motor0, &motor1);
-    }
     
     return 0;
 }
