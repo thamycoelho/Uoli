800b15d99945b185d8e21b0c4a4a6cf140649876
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-19 19:58:17 -0200

    test

--- segue-parede.c
+++ segue-parede.c
@@ -4,11 +4,17 @@ int _start(int argv, char** argc){
     motor_cfg_t motor0;
     motor_cfg_t motor1;
 
-    motor0.id = (unsigned char)0;
-    motor0.speed = (unsigned char)40;
+    motor0.id = 1;
+    motor0.id >>= 1;
 
-    motor1.id = (unsigned char)1;
-    motor1.speed = (unsigned char)40;
+    motor0.speed = 1;
+    motor0.speed <<= 3;
+
+
+    motor1.id = 1;
+
+    motor1.speed = 1;
+    motor1.speed <<=3;
 
     set_motors_speed(&motor0, &motor1);
     
