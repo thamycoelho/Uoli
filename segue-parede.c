81565ed7357c194b2d2e91feffc1d1796b5fe1e0
thamycoelho <thamycoelho10@gmail.com>
2017-11-21 20:56:34 -0200

    bla

--- segue-parede.c
+++ segue-parede.c
@@ -6,30 +6,30 @@ unsigned char sonar_id;
 unsigned short sonar_valor;
 
 int _start(int argv, char** argc){
-int i;
-    sonar_id = 4;
-    while(1){
-        sonar_valor = read_sonar(sonar_id);
-        if(sonar_valor < 20){
-            motor1.speed = 0;
-            motor0.speed = 0;
-            set_motors_speed(&motor0,&motor1);
-            break;
-        }
+//int i;
+  //  sonar_id = 4;
+    //while(1){
+      //  sonar_valor = read_sonar(sonar_id);
+        //if(sonar_valor < 20){
+          //  motor1.speed = 0;
+            //motor0.speed = 0;
+            //set_motors_speed(&motor0,&motor1);
+            //break;
+        //}
         
-        motor0.id = 0;
-      	motor0.speed = 40;
-    	motor1.id = 1;
-    	motor1.speed = 40;
+       // motor0.id = 0;
+      //	motor0.speed = 40;
+    	//motor1.id = 1;
+    	//motor1.speed = 40;
 	
-    	set_motors_speed(&motor0, &motor1);
+    	//set_motors_speed(&motor0, &motor1);
 	
 //    	if(read_sonar(sonar_id) <= 50){
   //          motor1.speed = 0;
     //        set_motor_speed(&motor1);
       //  }
             
-    }
+   // }
 
     while(1);
     return 0;
