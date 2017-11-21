247d9834b6fde9e3eec6414f1167eb20780c7776
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-21 17:38:56 -0200

    tempo read_sonar

--- segue-parede.c
+++ segue-parede.c
@@ -7,18 +7,20 @@ unsigned short sonar_valor;
 
 int _start(int argv, char** argc){
 int i;
+    sonar_id = 4;
     while(1){
         sonar_valor = read_sonar(sonar_id);
         if(sonar_valor < 20){
-            motor1.speed = 0
-            set_motor_speed(&motor1);
+            motor1.speed = 0;
+            motor0.speed = 0;
+            set_motors_speed(&motor0,&motor1);
+            break;
         }
         
         motor0.id = 0;
       	motor0.speed = 40;
     	motor1.id = 1;
     	motor1.speed = 40;
-    	sonar_id = 4;
 	
     	set_motors_speed(&motor0, &motor1);
 	
