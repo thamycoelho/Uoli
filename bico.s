444d505b7d99d4243f7bf562a6705c5fc449b0f3
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-22 11:30:54 -0200

    atualizando a api

--- bico.s
+++ bico.s
@@ -1,6 +1,12 @@
 .global set_motor_speed
 .global set_motors_speed
 .global read_sonar
+.global read_sonars
+.global register_proximity_callback
+.global add_alarm
+.global get_time
+.global set_time
+
 
 .data 
 
@@ -8,6 +14,7 @@
 .text
 .align 4
 
+@ Chama a syscall set_motor_speed
 set_motor_speed:
     ldrb r2, [r0]
     ldrb r1, [r0, #1]
@@ -17,6 +24,7 @@ set_motor_speed:
     svc 0x0
     mov pc, lr
 
+@ Chama a syscall set_motors_speed
 set_motors_speed:
     ldrb r2, [r0, #1]
     ldrb r3, [r1, #1]
@@ -28,8 +36,64 @@ set_motors_speed:
     svc 0x0
     mov pc, lr
 
+@ Chama a syscall read sonar
 read_sonar:
     
     mov r7, #16
     svc 0x0
     mov pc,lr
+
+@ Chama a syscal read_sonars
+read_sonars:
+    
+    ldr r3, [r2]
+    mov r2, r3      @r2 guarda o endereco de memoria do vetor de distancias
+    mov r3, r0      @r3 guarda o sonar inicial
+
+loop_sonars:
+    cmp r3, r1
+    bhi fim_loop_sonars
+    
+    mov r0, r3      @ Pega o sonar a ser lido e coloca em r0
+    
+    mov r7, #16   
+    svc 0x0         @ Chama a syscall
+        
+    str r0, [r2]    @ coloca o valor retornado pela syscall no vetor
+    add r2, #4      @ vai pra proxima posicao do vetor
+    add r3, #1      @ vai pro proximo sonar a ser lido
+    b loop_sonars:
+    
+fim_loop_sonars:
+    mov pc, lr
+
+@ Chama a syscall register_proximity_callback
+register_proximity_callback:
+
+
+
+
+
+@ Chama a syscall set_alarm
+add_alarm:
+
+
+
+
+@ Chama a syscall get_time
+get_time:
+    mov r1, r0
+    
+    mov r7, #20
+    svc 0x0
+
+    str r0, [r1]
+    mov pc, lr
+
+@ Chama a syscall set_time
+set_time:
+    
+    mov r7, #21
+    svc 0x0
+
+ 
