7587f5b7ba6be5c520c943f37a0c959bb11272f3
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-23 19:30:27 -0200

    lab10

--- segue-parede.c
+++ segue-parede.c
@@ -9,20 +9,20 @@ unsigned short sonar_valor2;
 unsigned int time;
 int i;
 
-void *vira_uoli(){
+/*void *vira_uoli(){
 
 
 }
-
+*/
 int _start(int argv, char** argc){
 
-   sonar_id = 4;
-   // motor0.id = 0;
-   // motor0.speed = 40;
-   // motor1.id = 1;
-   // motor1.speed = 40;
-    //set_motors_speed(&motor0, &motor1);
-    register_proximity_callback(sonar_id, 500, vira_uoli);
+    //sonar_id = 4;
+    motor0.id = 0;
+    motor0.speed = 40;
+    motor1.id = 1;
+    motor1.speed = 40;
+    set_motors_speed(&motor0, &motor1);
+    //register_proximity_callback(sonar_id, 500, vira_uoli);
 
     while(1);
     return 0;
