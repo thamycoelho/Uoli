cd55293f3e1f02770ec127b0f578755d84adacfa
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-23 19:08:27 -0200

    atualizando a api

--- segue-parede.c
+++ segue-parede.c
@@ -9,33 +9,19 @@ unsigned short sonar_valor2;
 unsigned int time;
 int i;
 
+void *vira_uoli();
+
 int _start(int argv, char** argc){
 
-   // sonar_id = 4;
+   sonar_id = 4;
    // motor0.id = 0;
    // motor0.speed = 40;
    // motor1.id = 1;
    // motor1.speed = 40;
     //set_motors_speed(&motor0, &motor1);
-
-    while(1){
-        get_time(&time);
-
-        for (i=0; i<100; i++){
-
-         }
-       //   sonar_valor1 = read_sonar(sonar_id);
-       // sonar_valor2 = read_sonar(sonar_id);
-        //sonar_valor = sonar_valor1 > sonar_valor2 ? sonar_valor1 : sonar_valor2;
-
-      //  if(sonar_valor <= 800){
-        //    motor1.speed = 0;
-          //  motor0.speed = 0;
-           // set_motors_speed(&motor0,&motor1);
-           // break;
-       // }
-    }
+    register_proximity_callback(sonar_id, 500, vira_uoli);
 
     while(1);
     return 0;
 }
+
