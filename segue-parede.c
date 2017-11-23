942a3032d39bd255ab7b2532348858d9c578cd9f
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-23 12:17:37 -0200

    atualizando a api

--- segue-parede.c
+++ segue-parede.c
@@ -8,28 +8,28 @@ unsigned short sonar_valor;
 int _start(int argv, char** argc){
     int i;
     sonar_id = 4;
+    motor0.id = 0;
+    motor0.speed = 40;
+    motor1.id = 1;
+    motor1.speed = 40;
     while(1){
-        sonar_valor = read_sonar(sonar_id);
-        //if(sonar_valor < 20){
-          //  motor1.speed = 0;
-            //motor0.speed = 0;
-            //set_motors_speed(&motor0,&motor1);
-            //break;
-        //}
-        
-       // motor0.id = 0;
-      //	motor0.speed = 40;
-    	//motor1.id = 1;
-    	//motor1.speed = 40;
-	
-    	//set_motors_speed(&motor0, &motor1);
-	
-//    	if(read_sonar(sonar_id) <= 50){
-  //          motor1.speed = 0;
-    //        set_motor_speed(&motor1);
+
+        if(sonar_valor <= 1200){
+            motor1.speed = 0;
+            motor0.speed = 0;
+            set_motors_speed(&motor0,&motor1);
+            break;
         }
-            
-   // }
+
+
+        set_motors_speed(&motor0, &motor1);
+
+        //    	if(read_sonar(sonar_id) <= 50){
+        //          motor1.speed = 0;
+        //        set_motor_speed(&motor1);
+    }
+
+    // }
 
     while(1);
     return 0;
