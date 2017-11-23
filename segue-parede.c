70f4bd3137aa8dd1870e04a8cd77c1dd9c430c99
Thamiris Coelho <thamycoelho10@gmail.com>
2017-11-23 14:29:45 -0200

    atualizando a api

--- segue-parede.c
+++ segue-parede.c
@@ -6,19 +6,20 @@ unsigned char sonar_id;
 unsigned short sonar_valor;
 unsigned short sonar_valor1;
 unsigned short sonar_valor2;
-
+unsigned int time;
 int _start(int argv, char** argc){
     int i;
 
-    sonar_id = 4;
-    motor0.id = 0;
-    motor0.speed = 40;
-    motor1.id = 1;
-    motor1.speed = 40;
+   // sonar_id = 4;
+   // motor0.id = 0;
+   // motor0.speed = 40;
+   // motor1.id = 1;
+   // motor1.speed = 40;
     //set_motors_speed(&motor0, &motor1);
 
     while(1){
-        sonar_valor1 = read_sonar(sonar_id);
+        get_time(&time);
+     //   sonar_valor1 = read_sonar(sonar_id);
        // sonar_valor2 = read_sonar(sonar_id);
         //sonar_valor = sonar_valor1 > sonar_valor2 ? sonar_valor1 : sonar_valor2;
 
