95c95e29fcd8798525290f11e4fd79c2f8a262ed
thamycoelho <thamycoelho10@gmail.com>
2017-11-26 01:24:36 -0200

    callback funcionando

--- SOUL.s
+++ SOUL.s
@@ -43,9 +43,9 @@ interrupt_vector:
 .text
 
 RESET_HANDLER:
-    
+
     @Zera o cotador
-    ldr r2, =CONTADOR
+    ldr r2, =contador
     mov r0, #0
     str r0, [r2]
     
@@ -53,7 +53,6 @@ RESET_HANDLER:
     ldr r0, =interrupt_vector
     mcr p15, 0, r0, c12, c0, 0
 
-    
     @habilita o clock_src no GPT
     ldr r0, =GPT_CR
     mov r1, #0x41
@@ -116,19 +115,20 @@ SET_TZIC:
     str r0, [r1, #TZIC_INTCTRL]
 
     msr  CPSR,  MODO_IRQ  @IRQ mode sem interrupcoes 
+    
+    @ inicializa pilha do modo IRQ
     ldr r0, =sp_irq
     mov sp, r0
 
+    @ Troca para o modo system e inicializa a pilha desse modo
     msr CPSR_c, #0x1F
     ldr sp, =sp_system
    
-    @ volta para o modo supervisor     
+    @ volta para o modo supervisor e inicializa a pilha do modo    
     msr CPSR_c, #0x13
+    ldr sp, =sp_super
 
-    ldr sp, =sp_SUPER
-
-    @ configura o GPIO
- 
+@ configura o GPIO
 SET_GPIO:
     @ Constantes para os enderecos do GPIO
     .set GPIO_DR,  0x53F84000
@@ -143,16 +143,15 @@ SET_GPIO:
     ldr r1, =GPIO_PINO
     str r1,[r0, #GPIO_GDIR]
       
-    @instrucao msr - habilita interrupcoes
-    msr  cpsr, #0x10       @ USER mode
+    @ Troca de modo, para o modo usuario e  habilita interrupcoes
+    msr  cpsr, #0x10       
  
     @ TRANSFERE  o fluxo para o codigo do usuario
     .set COD_USER, 0x77812000
-    
-    @ldr sp, =sp_user
     ldr r0, =COD_USER
     bx r0
-   
+
+@ Tratando interrupcoes do tipo IRQ   
 IRQ_HANDLE:
     push {r0-r12,lr}
     
@@ -165,30 +164,60 @@ IRQ_HANDLE:
     mov r1, #0x1
     str r1, [r0]
 
+@ Trata as callbacks programadas
 callback:
-    @ Percorre o vetor de callbacks
+    @ Verifica a flag de callbaks para verificar se esta ocorrendo uma chamada de callback no momento
+    ldr r0, =callflag
+    ldr r1, [r0]
+    cmp r1, #1
+    
+    @ Caso esteja ocorrendo uma callback entao termina de tratar a callback que esta ocorrendo, caso contrario percorre o vetor de callbacks 
+    beq fim_percorre_vetor_callback
+   
+    @ Verifica a quantidade de callbacks existentes, se houver mais de uma percorre o vetor
     ldr r0, =qtd_callback
     ldr r1, [r0]
     mov r2, #0
+
 percorre_vetor_callback:
     cmp r2, r1
     bhs fim_percorre_vetor_callback
+
+    @ Corrige a varialvel do loop para a variacao de endereco correta
     mov r3, r2, lsl #2
+
+    @ r0 recebe o id do sonar que deve ser lida a distancia
     ldr r4, =callback_sonar_vector
     ldr r0, [r4, r3]
     
+    @ Chama syscall para leiitura do sonar
     mov r7, #16
     svc 0x0
     
+    @ r5 recebe a distancia necessaria para chamada da funcao
     ldr r4, =callback_distance_vector
     ldr r5, [r4, r3]
     
+    @ Se a distancia for menor ou igual, chama a funcao da callback
     cmp r0, r5
     ldrls r4, =callback_function_vector
     ldrls r5, [r4, r3]
-    push {r1-r11,lr}
-    blxls r5
-    pop {r1-r11,lr}
+
+    @ Se for realizar a chamada de funcao, indica na flag que esta ocorendo uma callback
+    ldrls r4, =callflag
+    movls r6, #1
+    strls r6, [r4]
+
+    push {r1-r11,lr}    @ Empilha os registradores 
+    blxls r5            @ Chama a funcao requerida pela callback
+    pop {r1-r11,lr}     @ Desempilha os registradores
+
+    @ Zera novamente a flag de callbacks 
+    ldr r4, =callflag
+    mov r5, #0
+    str r5, [r4]
+    
+    @ Continua percorrendo o vetor
     add r2, #1
     b percorre_vetor_callback
 fim_percorre_vetor_callback: 
@@ -221,23 +250,25 @@ percorre_vetor_alarm:
     add r2, #1 
     b percorre_vetor_alarm
 fim_perccore_vetor_alarm:
-
     
     @ Incrementando o contador em uma unidade
-    ldr r0, =CONTADOR
+    ldr r0, =contador
     ldr r1, [r0]
     add r1, #1
     str r1, [r0]
     
+    @ Restaurando SPSR e os demais registradores
     pop {r0}
     msr SPSR, r0
     pop {r0-r12,lr}
-    @ Subtraindo em 4 unidades o LR
+
+    @ Subtrai em 4 unidades o LR e retorna da funcao
     sub lr, #4
     movs pc, lr 
     
 @ Tratamento das Syscalls
 SYSCALL_HANDLE:
+    @ Troca pro modo supervisor sem perder o SPSR do modo do usuario
     mrs r11, SPSR
     push {r11}
 
@@ -246,6 +277,7 @@ SYSCALL_HANDLE:
     pop {r11}
     msr SPSR, r11
     
+    @ Empilha SPSR e os demais registradores
     push {r1-r12, lr}
     mrs r12, SPSR
     push {r12}
@@ -256,7 +288,6 @@ SYSCALL_HANDLE:
     .equ MAX_ALARM, 8
     
     @ Verifica qual a syscall chamada    
-    
     cmp r7, #16
     beq read_sonar
     cmp r7, #17
@@ -272,6 +303,7 @@ SYSCALL_HANDLE:
     cmp r7, #22
     beq set_alarm
 
+    @ Caso a chamada tenha sido feita para uma syscall invalida, retorna
     pop {r12}
     msr SPSR, r12
     pop {r1-r12, lr}
@@ -283,24 +315,25 @@ SYSCALL_HANDLE:
 @ r1: Velocidade
 @ Retorno:
 @ r0: -1 caso o identificador do motor seja invalido, -2 caso a velocidade seja invalida, 0 caso aplicou a velocidade
-    
 set_motor_speed:
     @ Mascara para pegar a velocidade do motor e setar o motor_write
     .set SET_MOTOR0,    0x01FC0000
     .set SET_MOTOR1,    0xFE000000
-     
-    @ caso a velocidade do motor tenha mais que 6 bits (valor maximo 63), entao retorna com r1 = -2
+    
+    @ Caso o id do motor seja invalido, retorna -1
     cmp r0, #1
     movhi r0, #-1
     bhi fim_set_motor_speed
     
+    @ Caso a velocidade do motor tenha mais que 6 bits (valor maximo 63), entao retorna -2
     cmp r1, #63
     movhi r0, #-2
     bhi fim_set_motor_speed
 
-    @ verifica se vai setar o motor 0
+    @ verifica se vai setar o motor 0 ou o motor 1
     cmp r0, #0
 
+    @ Coloca a velocidade do motor desejado no registrador GPIO_DR
     mov r2, #0
     orr r2, r2, r1, lsl #1
     ldr r0, =GPIO_DR
@@ -311,6 +344,7 @@ set_motor_speed:
     orrne r1, r1, r2, lsl #25
     str r1, [r0]
     
+    @ Caso tenha setado as velocidades, retorna 0
     mov r0, #0
     
 fim_set_motor_speed:    
@@ -325,7 +359,6 @@ fim_set_motor_speed:
 @ r1: velocidade para o motor 1
 @ retorno:
 @ r0: -1 caso a velocidade do motor 0 seja invalida, -2 caso a velocidade do motor 1 seja invalida, 0 caso definiu as velocidades
-
 set_motors_speed:
     @ Mascara para aplicar a velocidade dos motores
     .set SET_MOTORS, 0xFFFC0000
@@ -340,8 +373,8 @@ set_motors_speed:
     
     @ coloca em r2 os valores em sequencia dos bits de escrita e as velocidades dos motores
     mov r2, #0
-    orr r2, r0, r0, lsl #1
-    orr r2, r1, r1, lsl #7
+    orr r2, r2, r0, lsl #1
+    orr r2, r2, r1, lsl #8
     
     @ coloca no GPIO_DR os valores necessarios
     ldr r0, =GPIO_DR
@@ -362,12 +395,13 @@ fim_set_motors_speed:
 @ Funcao retorna o tempo do sistema
 @ Retorno:
 @ r0: tempo do sistema
-
 get_time:
-    ldr r0, =CONTADOR
+    @ Coloca em r0 o tempo do sistema
+    ldr r0, =contador
     ldr r1, [r0]
     mov r0, r1
     
+    @ Retorna da syscall
     pop {r12}
     msr SPSR, r12
     pop {r1-r12, lr}
@@ -376,11 +410,12 @@ get_time:
 @ Funcao define um tempo para o sistema
 @ Parametros:
 @ r0: tempo do sistema
-
 set_time:
-    ldr r1, =CONTADOR
+    @ Inicializa o contador com um valor
+    ldr r1, =contador
     str r0, [r1]
 
+    @ Retorna da Syscall
     pop {r12}
     msr SPSR, r12
     pop {r1-r12, lr}
@@ -391,16 +426,17 @@ set_time:
 @ r0: Identificador do sonar
 @ Retorno:
 @ r0: -1 caso o identificador do sonar seja invalido, valor lido no sonar caso seja um sonar valido
-
 read_sonar:
     
     @ mascara para modificar o MUX em GPIO_DR
     .equ SONARES, 0x3E
     .equ LER_SONARES, 0x3FFC0
 
+    @ Verifica se o sonar eh valido
     cmp r0, #15
     movhi r0, #-1
     bhi fim_read_sonar 
+
     @ r2 contem o conteudo de GPIO_DR
     ldr r1, =GPIO_DR
     ldr r2, [r1]
@@ -410,6 +446,7 @@ read_sonar:
     orr r2, r2, r0, lsl #2
     str r2, [r1]
 
+@ Loop para passagem de tempo
     mov r3, #0
 for_time1:
     cmp r3, #50
@@ -423,6 +460,7 @@ fim_for_time1:
     orr r2, r2, r3
     str r2, [r1]
 
+@ Loop para passagem de tempo
     mov r3, #0
 for_time2:
     cmp r3, #50
@@ -436,7 +474,10 @@ fim_for_time2:
     bic r2, #2 
     str r2, [r1]
     
+@ Aguarda a flag setar o bit
 loop:
+
+@ Loop para passagem de tempo
     mov r3, #0
 for_time3:
     cmp r3, #50
@@ -460,70 +501,104 @@ fim_loop:
     and r0, r0, r2
     mov r0, r0, lsr #6
 
+@ Retorna da syscall
 fim_read_sonar:
     pop {r12}
     msr SPSR, r12
     pop {r1-r12, lr}
     movs pc, lr
 
+@ Adiciona um alarme
+@ Parametros:
+@ r0: Ponteiro para funcao a ser chamada na ocorrencia de alarme
+@ r1: tempo do sistema
+@ Retorno:
+@ r0: -1 caso o numero de alarmes seja maior que o permitido, -2 caso o tempo do alarme seja anterior ao tempo do sistema, 0 caso o alarme foi adicionado
 set_alarm:
+    @ retorna -1 caso a quantidade de alarmes ja tenham esgotado
     ldr r2, =qtd_alarm
     ldr r3, [r2]
     cmp r3, #MAX_ALARM
     movhs r0, #-1
     bhs fim_alarm
     
-    ldr r2, =CONTADOR
+    @ Verifica o tempo do sistema e compara com o tempo do alarme
+    ldr r2, =contador
     ldr r3, [r2]
     cmp r1, r3
     movlo r0, #-2
     blo fim_alarm
     
+    @ Arruma o deslocamento dos vetores de alarme
     ldr r2, =qtd_alarm
     ldr r3, [r2]
     mov r3, r3, lsl #2
+
+    @ Coloca o tempo da ocorrencia do alarme no vetor
     ldr r2, =alarm_time_vector
     str r1, [r2, r3]
+
+    @ Coloca a funcao a ser chamada no alarme no vetor
     ldr r2, =alarm_function_vector
     str r0, [r2, r3]
     
-
+    @ Adiciona 1 na quantidade de alarmes
     ldr r2, =qtd_alarm
     ldr r3,[r2]
     add r3, #1
     str r3, [r2]
+
+@ Retorna da syscall
 fim_alarm:
     pop {r12}
     msr SPSR, r12
     pop {r1-r12, lr}
     movs pc, lr
     
-
+@ Adiciona uma callback
+@ Parametros:
+@ r0: id do sonar a ser lido
+@ r1: Distancia para a ocorrencia do callback
+@ r3: Ponteiro para a funcao chamada pela callback
+@ Retornos:
+@ r0: -1 caso o numero de callbacks ja estaja no limite, -2 caso o id do sonar seja invalido, 0 caso tenha adicionado o alarme
 register_proximity_callback:
+    @ Verifica se o sonar eh valido
     cmp r0, #15
     movhi r0, #-2
     bhi callback_fim
+    
+    @ Verifica a quantidade de callbacks
     ldr r3, =qtd_callback
     ldr r4, [r3]
     cmp r4, #MAX_CALLBACK
     movhs r0, #-1
     bhs callback_fim
-
+    
+    @ Pega o deslocamento que deve ser feito ao armazenar dados no vetor
     ldr r3, =qtd_callback
     ldr r4, [r3]
     mov r4, r4, lsl #2
+
+    @ Coloca o id do sonar no vetor
     ldr r3, =callback_sonar_vector
     str r0, [r3, r4]
+
+    @ Coloca a distancia desejada  no vetor
     ldr r3, =callback_distance_vector
     str r1, [r3, r4]
+
+    @ Coloca o ponteiro para a funcao no vetor
     ldr r3, =callback_function_vector
     str r2, [r3, r4]
 
+    @ Adiciona 1 na quantidade de callbacks
     ldr r3, =qtd_callback
     ldr r4, [r3]
     add r4, #1
     str r4, [r3]
     
+@ Retorna da syscall
 callback_fim:
     pop {r12}
     msr SPSR, r12
@@ -531,23 +606,23 @@ callback_fim:
     movs pc, lr
 
 .data 
+
+@ Flag para controlar chamadas de callback
+callflag:
+    .word 0x0
 @ Alocando espaco para o contador
-CONTADOR:
+contador:
     .word 0x0
 
-@ Alocando um espaco para a pilha de IRQ
-pilha_IRQ:
+@ Alocando um espaco para as pilhas do sistema
+pilha_irq:
     .skip 1024
 sp_irq:
-  
+
 pilha_super:
     .skip 1024
-sp_SUPER:
+sp_super:
     
-pilha_user:
-    .skip 1024
-sp_user:
-
 pilha_system:
     .skip 1024
 sp_system:
@@ -560,11 +635,15 @@ callback_distance_vector:
 callback_function_vector:
     .space 32
 
+@ Variavel que contem a quantidade de callbacks
 qtd_callback:
     .word 0x0
 
+@ Variavel que contem a quantidade de alarmes
 qtd_alarm:
     .word 0x0
+
+@ Alocando espaco para os vetores de alarmes
 alarm_function_vector:
     .space 32
 alarm_time_vector:
--- segue-parede.c
+++ segue-parede.c
@@ -2,40 +2,59 @@
 
 motor_cfg_t motor0;
 motor_cfg_t motor1;
-unsigned char sonar_id;
-unsigned short sonar_valor;
-unsigned short sonar_valor1;
-unsigned short sonar_valor2;
-unsigned int time;
-unsigned int nextTime;
-int i;
+int found_wall;
+unsigned short sonar[2];
 
-void *vira_uoli();
+void *ajusta_parede();
+int abs(int a);
 
 int _start(int argv, char** argc){
-
-    sonar_id = 4;
-    register_proximity_callback(sonar_id, 1200, vira_uoli);
     motor0.id = 0;
     motor0.speed = 40;
     motor1.id = 1;
     motor1.speed = 40;
+
+    found_wall = 0;
+
     set_motors_speed(&motor0, &motor1);
-    
+
+    register_proximity_callback(4, 1500, ajusta_parede);
+/*
+    while(1){
+        sonar[0] = read_sonar(0);
+        sonar[1] = read_sonar(15);
+
+        if(abs(sonar[0] - sonar[1]) > 50){
+            if(sonar[0] > sonar[1]){
+                motor1.speed = 35;
+                set_motor_speed(&motor1);
+            }
+            else{
+                motor0.speed = 35;
+                set_motor_speed(&motor0);
+            }
+        }
+        else{
+            motor0.speed = motor1.speed = 40;
+            set_motor_speed(&motor0);
+            set_motor_speed(&motor1);
+        }
+    }
+    */
     while(1);
     return 0;
 }
 
-void *vira_uoli(){
+void *ajusta_parede(){
     motor0.speed = 0;
     set_motor_speed(&motor0);
 
-    get_time(&time);
-    nextTime = time + 3;
-    while(time != nextTime)
-         get_time(&time);
+    while(read_sonar(0) > 1200);
 
     motor0.speed = 40;
     set_motor_speed(&motor0);
 }
 
+int abs(int a){
+    return a < 0 ? -a : a;
+}
